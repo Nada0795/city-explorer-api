6ddb72efeb1870ecaf8528753424a4526a38fd87
Nada0795 <nadakh1993@icloud.com>
2021-08-07 01:51:27 +0300

    adding notes

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 'use strict';
 
 require('dotenv').config();
-const cors = require('cors');
 const express = require("express");
 const server = express();
+const cors = require('cors');
 const data =require('./data/wether.json');
 
 const PORT = process.env.PORT;
+server.use(cors());
 
 // const PORT = 3001
 
@@ -14,10 +15,21 @@ const PORT = process.env.PORT;
 // const lon = process.data.lon;
 // const searchQuery = process.data.city_name;
 
+//http://localhost:3001/weather?lat=31.95&lon=35.91&searchQuery=Amman
+server.get('/weather',(req,res)=>{
+const lat = Number (req.query.lat);
+const lon = Number (req.query.lon);
+const cityName=req.query.searchQuery.toLowerCase();
+
+console.log(lat,lon,cityName);
+const result = data.find(item=>{
+  
+} )
+
+result ? res.send(result) : res.status(500).send ('anything')
+})
 
 
-
-server.use(cors());
 
 
 server.listen(PORT, () => {
