27ff70884c765b22f480ca314118c182dd6c67b3
Nada0795 <nadakh1993@icloud.com>
2021-08-14 00:03:19 +0300

    lab8D

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # city-explorer-api
+
+* .env content : 
+PORT = 3001
+WEATHER_API_KEY = dfd8a348f5bc4e489e5a4d52dc69a983
--- server.js
+++ server.js
@@ -1,104 +1,135 @@
-'use strict';
+'use strict'
 
-require('dotenv').config();
-const express = require("express");
-const server = express();
-const axios = require("axios");
-const cors = require('cors');
-const data =require('./data/wether.json');
-
+const express = require ("express"); // import express library using require // npm i express
+const server =express();
+// // import json file
+// const weatherData = require('./data/weather.json'); // import json file
+require('dotenv').config(); // .env // npm i dotenv
+const cors = require('cors'); // cors // npm i cors
+server.use(cors()); // the server can take any req from any client
 const PORT = process.env.PORT;
-server.use(cors());
+const axios = require('axios');
 
-//http://localhost:3001/getPhotos?name=cities
-server.get('/getphoto',getPhotoHandler)
-
-
-async function getPhotoHandler(req,res){
-  const query=req.query.name;
-  const URL = `https://api.unsplash.com/photos?query=${query}&client_id=${process.env.UnSplash_Key}`;
-
-  // console.log(req.query);
-    // try{
-    //   const result =await axios.get(URL);
-    //   // console.log(result)
-    //   res.send(result.data);
-  
-    // }
-    // catch (error){
-    //   res.send(error);
-    // }
-    console.log('before promise');
-  axios
-  .get(URL)
-  .then(result=>{
-    console.log('inside promise');
-let photoArray=result.data.map(item=>{
-  return new photo(item);
+// http://localhost:3001/ (/ === root route)
+server.get('/',(req,res) =>{ // we can call the req,res 
+res.send('hi from the root route'); 
 })
 
-   res.send(photoArray);
-  })
-.catch(eror=>{
-  res.send(error);
+//===================================================lab8====================================================
+
+//http:localhost:3001/getWeather?lat=31.95&lon=35.91&searchQuery=Amman
+server.get('/getWeather', getWeatherHandler);
+
+async function getWeatherHandler(req, res) {
+    const city = req.query.searchQuery
+    const lon = req.query.lon
+    const lat = req.query.lat
+
+    //${process.env.API_KEY}
+    const URL = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`;
+
+
+    axios
+        .get(URL)
+        .then(result => {
+            console.log('inside promise');
+            let weatherArray = result.data
+            res.send(weatherForObject(weatherArray));
+        })
+        .catch(err => {
+            res.send(err);
+        })
+    console.log('outside promise');
+}
+
+const weatherForObject = (weatherObj) => {
+
+    const forCastObj = [];
+    weatherObj.data.map(element => {
+
+        const description = `Low of ${element.low_temp} ,High of ${element.max_temp} with ${element.weather.description}`;
+        const date = element.datetime;
+        forCastObj.push(new Forcast(description, date));
+        console.log(forCastObj);
+    });
+    return forCastObj;
+};
+class Forcast {
+    constructor(description, date) {
+        this.date = date;
+        this.description = description;
+
+    }
+}
+
+//http:localhost:3001/movies?city=Amman
+server.get('/movies', getMovieHandler);
+
+function getMovieHandler(req, res) {
+    const city = req.query.city
+
+    //https://api.themoviedb.org/3/search/movie?api_key=6680ffabd529834b6beac25752cbb0ad&query=amman
+    const URLMovie = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${city}`
+
+    axios
+        .get(URLMovie)
+        .then(result => {
+            console.log('inside promise');
+
+            let moviesArray = result.data.results
+
+            res.send(moviesForObject(moviesArray));
+        })
+        .catch(err => {
+            res.send(err);
+        })
+    console.log('outside promise');
+}
+
+
+const moviesForObject = (moviesObj) => {
+
+    const forMoviesObj = [];
+    moviesObj.map(element => {
+
+    const title = element.title
+    const overview = element.overview
+    const vote_average = element.vote_average
+    const vote_count = element.vote_count
+    const poster_path = process.env.img_url+element.poster_path
+    const popularity = element.popularity
+    const release_date = element.release_date
+
+        forMoviesObj.push(new Movies(title,overview,vote_average,vote_count,poster_path,popularity,release_date));
+
+        console.log(forMoviesObj);
+    });
+    return forMoviesObj;
+}
+
+
+class Movies { // movie class
+    constructor(title,overview,vote_average,vote_count,poster_path,popularity,release_date) {
+    this.title = title
+    this.overview = overview
+    this.vote_average = vote_average
+    this.vote_count = vote_count
+    this.poster_path = poster_path
+    this.popularity = popularity
+    this.release_date = release_date
+    }
+}
+
+
+
+// any other routes
+server.get('*', (req,res)=> {
+    res.status(404).send('page not found');
 })
-console.log('outside promise');
-}
-
-class photo {
-  constructor(item){
-    this.imgUrl = item.urls.raw
-    this.numLikes=item.likes
-  }
-}
 
 
-class Forecast {
-constructor(date,description){
-  this.date=date;
-  this.description=description;
+// listen, in the end of our code
+server.listen(PORT,() =>{ // callback fun, when u listen to the port and got the request run this callback func
+console.log(`Im listning on PORT ${PORT}`);
 
-}
-
-
-}
-// const PORT = 3001
-
-// const lat = process.data.lat;
-// const lon = process.data.lon;
-// const searchQuery = process.data.city_name;
-
-// "description": "Low of 17.1, high of 23.6 with broken clouds",
-// "date": "2021-03-31"
-
-//http://localhost:3001/weather?lat=31.95&lon=35.91&searchQuery=Amman
-server.get('/weather',(req,res)=>{
-const lat = Number (req.query.lat);
-const lon = Number (req.query.lon);
-const cityName=req.query.searchQuery.toLowerCase();
-
-console.log(lat,lon,cityName);
-const result = []
-data.find(item=>{
-  if (item.city_name.toLowerCase()==cityName){
-item.data.forEach(day => {
-
-  const desc=`Low of ${day.low_temp}, high of ${day.high_temp},with ${day.weather.description}`
-console.log(day)
-result.push( new Forecast (day.datetime,desc))
-  
-});
-
-
-  }
-} )
-
-result ? res.send(result) : res.status(500).send ('anything')
 })
-
-
-
-
-server.listen(PORT, () => {
-  console.log(`im listening on PORT = ${PORT}`);
-});
