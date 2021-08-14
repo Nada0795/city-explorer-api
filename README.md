67a44452ea56cbb547ea722e2ba2b3a7a88ac6a2
Nada0795 <nadakh1993@icloud.com>
2021-08-14 04:13:43 +0300

    solving lab8

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # city-explorer-api
+
+* .env content :
+
+PORT=3001
+
+WEATHER_API_KEY = pkb2d9bf0052a0428c7de26a517415f6f9
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const server = express();
 const cors = require('cors');
 const data =require('./data/wether.json');
-
+const axios = require('axios');
 const PORT = process.env.PORT;
 server.use(cors());
 
@@ -18,6 +18,20 @@ constructor(date,description){
 
 
 }
+
+axios
+.get(URL)
+.then(result => {
+    // console.log('inside promise');
+    let weatherArray = result.data
+    res.send(weatherForObject(weatherArray));
+})
+.catch(err => {
+    res.send(err);
+})
+// console.log('outside promise');
+}
+
 // const PORT = 3001
 
 // const lat = process.data.lat;
@@ -52,9 +66,74 @@ result.push( new Forecast (day.datetime,desc))
 result ? res.send(result) : res.status(500).send ('anything')
 })
 
+//http:localhost:3001/movies?city=Amman
+server.get('/movies', getMovieHandler);
+
+function getMovieHandler(req, res) {
+    const city = req.query.city
+
+
+ //https://api.themoviedb.org/3/movie/550?api_key=5da0fe89faab93fafb9d23776003ee8b&query=amman
+    const URLMovie = `https://api.themoviedb.org/3/movie/550?api_key=${process.env.MOVIE_API_KEY}&query=${city}`
 
 
 
+   
+    axios
+    .get(URLMovie)
+    .then(result => {
+        // console.log('inside promise');
+
+        let moviesArray = result.data.results
+
+        res.send(moviesForObject(moviesArray));
+    })
+    .catch(err => {
+        res.send(err);
+    })
+// console.log('outside promise');
+}
+
+const moviesForObject = (moviesObj) => {
+
+  const forMoviesObj = [];
+  moviesObj.map(element => {
+
+  const title = element.title
+  const overview = element.overview
+  const vote_average = element.vote_average
+  const vote_count = element.vote_count
+  const poster_path = process.env.img_url+element.poster_path
+  const popularity = element.popularity
+  const release_date = element.release_date
+
+
+  forMoviesObj.push(new Movies(title,overview,vote_average,vote_count,poster_path,popularity,release_date));
+
+  // console.log(forMoviesObj);
+});
+return forMoviesObj;
+}
+
+
+class Movies { // movie class
+  constructor(title,overview,vote_average,vote_count,poster_path,popularity,release_date) {
+  this.title = title
+  this.overview = overview
+  this.vote_average = vote_average
+  this.vote_count = vote_count
+  this.poster_path = poster_path
+  this.popularity = popularity
+  this.release_date = release_date
+  }
+}
+
+//any route
+server.get('*', (req,res)=> {
+  res.status(404).send('page not found');
+})
+
+//listen
 server.listen(PORT, () => {
   console.log(`im listening on PORT = ${PORT}`);
 });
